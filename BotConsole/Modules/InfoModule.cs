3af8c07ed9d0e9c51343176c654c0d84bfd8f74a
CSTECH\armagan.pekatik <armagan.pekatik@ciceksepeti.com>
2017-10-31 10:47:31 +0300

    console app addded

---
+++ BotConsole/Modules/InfoModule.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace BotConsole.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        // ~say hello -> hello
+        [Command("say")]
+        [Summary("Echos a message.")]
+        public async Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync(echo);
+        }
+    }
+}
---
+++ BotConsole/Modules/PingModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace BotConsole.Modules
+{
+
+    public class PingModule : ModuleBase<SocketCommandContext>
+    {
+        // ~say hello -> hello
+        [Command("ping")]
+        [Summary("pong")]
+        public async Task SayAsync()
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync("pong");
+        }
+    }
+}
