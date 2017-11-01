2652a59f006e3cd47cab24890f697e9a4f425fe9
CSTECH\armagan.pekatik <armagan.pekatik@ciceksepeti.com>
2017-11-01 14:56:27 +0300

    LPR integration

---
+++ Bot/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    public class Variables
+    {
+        public static string deger = "asdasdsa";
+    }
+}
---
+++ LPR/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace LPR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.processTimeLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(15, 97);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(392, 375);
+            this.panel1.TabIndex = 1;
+            // 
+            // processTimeLabel
+            // 
+            this.processTimeLabel.AutoSize = true;
+            this.processTimeLabel.Location = new System.Drawing.Point(12, 72);
+            this.processTimeLabel.Name = "processTimeLabel";
+            this.processTimeLabel.Size = new System.Drawing.Size(35, 13);
+            this.processTimeLabel.TabIndex = 2;
+            this.processTimeLabel.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(413, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 437);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(932, 484);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.processTimeLabel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label processTimeLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
